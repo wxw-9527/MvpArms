eedd64d412291aeccd4c273773d67ffcbf1078e4
xingwang <Saxxhw@gmail.com>
2023-03-10 11:45:41 +0800

    1.4.7 feature-增加JsonElementKt相关扩展方法

---
+++ arms/src/main/java/com/rouxinpai/arms/extension/JsonElementKt.kt
@@ -0,0 +1,34 @@
+package com.rouxinpai.arms.extension
+
+import com.google.gson.JsonObject
+
+/**
+ * author : Saxxhw
+ * email  : xingwangwang@cloudinnov.com
+ * time   : 2023/3/10 11:18
+ * desc   :
+ */
+
+infix fun String.eq(that: String): JsonObject {
+    val jsonObject = JsonObject()
+    jsonObject.addProperty("fieldName", this)
+    jsonObject.addProperty("operation", "eq")
+    jsonObject.addProperty("value", that)
+    return jsonObject
+}
+
+infix fun String.between(that: String): JsonObject {
+    val jsonObject = JsonObject()
+    jsonObject.addProperty("fieldName", this)
+    jsonObject.addProperty("operation", "between")
+    jsonObject.addProperty("value", that)
+    return jsonObject
+}
+
+infix fun String.`in`(that: String): JsonObject {
+    val jsonObject = JsonObject()
+    jsonObject.addProperty("fieldName", this)
+    jsonObject.addProperty("operation", "between")
+    jsonObject.addProperty("value", that)
+    return jsonObject
+}
